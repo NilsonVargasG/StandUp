d37051ab8e6a25e25188a3c75d2fa8ec87dbbdad
Nilson Vargas <nilsonvargas136@gmail.com>
2020-07-07 14:37:23 -0500

    Agregando pagina para crear nueva cuenta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'new-account',
+    loadChildren: () => import('./new-account/new-account.module').then( m => m.NewAccountPageModule)
+  },
 ];
 
 @NgModule({
